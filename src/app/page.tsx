ddaced7223539bdb2e641d036f0cb287e94d169b
Samip Chapagain <samipchapagain5@gmail.com>
2025-08-28 09:27:53 +0000

    change the color to match the rest of the buttons (blue)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
           <Button asChild size="lg">
             <Link href="/report">{t('home.fileReport')}</Link>
           </Button>
-           <Button asChild size="lg" variant="secondary">
+           <Button asChild size="lg">
             <Link href="/grievance">{t('home.voiceGrievance')}</Link>
           </Button>
           <Button asChild size="lg">
