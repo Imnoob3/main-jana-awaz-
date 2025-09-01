262feaff7447abd7826a1fdbff18893dc69ca14b
Samip Chapagain <samipchapagain5@gmail.com>
2025-09-01 20:02:22 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,9 @@
 
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseUrl = 'https://jkhhvfcrwfwlaaarmrlb.supabase.co';
+// This key is safe to use in a browser if you have enabled Row Level Security for your tables and configured policies.
+// It is recommended to use an environment variable for this.
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpraGh2ZmNyd2Z3bGFhYXJtcmxiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY3NTMwMzQsImV4cCI6MjA3MjMyOTAzNH0.Dzyfk2eBTaLHqwC0IVReOImf2j8p0VuUZNIJUB8_t3w";
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey);
