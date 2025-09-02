8c6ebc5f7bfe25b5e76ea379ba5d557aed9495d3
Evocative <sambhavkharel11@gmail.com>
2025-09-02 01:52:46 +0000

    its showing error why

--- src/app/report/schema.ts
+++ src/app/report/schema.ts
@@ -2,12 +2,12 @@
 import { z } from "zod";
 
 export const reportSchema = z.object({
-  reportText: z.string().min(50, { message: 'Report must be at least 50 characters long.' }),
+  reportText: z.string().min(50, { message: 'Report must be at least 50 characters long.' }).optional(),
   photoDataUri: z.string({required_error: "A photo is required."}).min(1, { message: 'A photo is required.' }),
   crimeType: z.enum(['government', 'civilian'], { required_error: 'You must select a crime type.' }),
-  crimeSubType: z.string({ required_error: 'You must select a specific crime type.' }),
+  crimeSubType: z.string({ required_error: 'You must select a specific crime type.' }).optional(),
   district: z.string({ required_error: 'You must select a district.' }),
-  localAddress: z.string().min(3, { message: 'Local address must be at least 3 characters long.' }),
+  localAddress: z.string().min(3, { message: 'Local address must be at least 3 characters long.' }).optional(),
 });
 
 export type FormState = {
